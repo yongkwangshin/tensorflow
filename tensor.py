ebb71936b8cf1f7bc1825d3f7b54cdd9be7d3d32
Sung Kyu Lim <limsk@ece.gatech.edu>
2017-12-22 17:58:28 -0500

    Add files via upload

--- tensor.py
+++ tensor.py
@@ -4,27 +4,32 @@
 # 12/15/2017
 
 # B1: import tensorflow
-import tensorflow as tf
-
-# B2: turn off warning sign
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
 
-# B3: placeholder and variable
-X = tf.placeholder(tf.float32, [None, 3])
-W = tf.Variable(tf.random_normal([3, 2]))
-b = tf.Variable([0.5, -0.5])
+# B2: placeholder
+x = tf.placeholder(tf.float32, [None, 3])
 
-# B4: output calculation
-output = tf.matmul(X, W) + b
+# B3: variable: we can initialize them using desired values
+W = tf.Variable([[0.1, 0.1], [0.1, 0.1], [0.1, 0.1]])
+b = tf.Variable([0.2, 0.2])
 
-# B5: main session
+# B4: or we can initialize them randomly
+# random_normal default: mean=0.0 and stddev=1.0
+# W = tf.Variable(tf.random_normal([3, 2]))
+# b = tf.Variable(tf.random_normal([2, 1]))
+
+# B5: output calculation
+output = tf.matmul(x, W) + b
+
+# B6: main session
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 input = [[1, 2, 3], [4, 5, 6]]
 
-print("input:", input, '\n')
-print("W:", sess.run(W), '\n')
-print("b:", sess.run(b), '\n')
-print("output:", sess.run(output, feed_dict={X: input}))
-sess.close()
+print("input:", input)
+print("W:", sess.run(W))
+print("b:", sess.run(b))
+print("output:", sess.run(output, feed_dict={x: input}))
+
