4e50467ba00d1bc64d7e04dc574b84807f49e210
Sung Kyu Lim <limsk@ece.gatech.edu>
2017-12-22 15:11:06 -0500

    Add files via upload

--- const.py
+++ const.py
@@ -6,21 +6,17 @@
 # B1: import tensorflow
 import tensorflow as tf
 
-# B2: turn off warning sign
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-
-# B3: string constant
+# B2: string constant
 hello = tf.constant('hello world!')
 print(hello)
 
-# B4: integer constant
+# B3: integer constant
 a = tf.constant(1)
 b = tf.constant(2)
 c = tf.add(a, b)  
 print(c)
 
-# B5: main session
+# B4: main session
 sess = tf.Session()
 print(sess.run(hello))
 print(sess.run([a, b, c]))
