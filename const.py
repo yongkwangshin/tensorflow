9adba47d1a97423db8181133c7544064910aa9dd
Sung Kyu Lim <limsk@ece.gatech.edu>
2017-12-22 17:06:15 -0500

    Add files via upload

--- const.py
+++ const.py
@@ -1,23 +1,38 @@
-# const.py: constants in tensorflow
+# const.py: constant arithmetic in tensorflow
 # written by Sung Kyu Lim
 # limsk@ece.gatech.edu
 # 12/15/2017
 
-# B1: import tensorflow
+# B1: simple add
+print(2+3)
+
+# B2: import tensorflow
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
-# B2: string constant
-hello = tf.constant('hello world!')
-print(hello)
+# B3: tensorflow add
+a = tf.constant(2)
+b = tf.constant(3)
+x = tf.add(a, b)
+with tf.Session() as sess:
+    print(sess.run(x))
 
-# B3: integer constant
-a = tf.constant(1)
-b = tf.constant(2)
-c = tf.add(a, b)  
-print(c)
+# B4: tensor flow multiply
+a = tf.constant([2, 2])
+b = tf.constant([[0, 1], [2, 3]])
+x = tf.add(a, b)
+y = tf.multiply(a, b)
+with tf.Session() as sess:
+    x, y = sess.run([x, y])
+    print('x:', x)
+    print('y:', y)
 
-# B4: main session
-sess = tf.Session()
-print(sess.run(hello))
-print(sess.run([a, b, c]))
-sess.close()
+# B5: tensor flow matrix multiply
+a = tf.constant([[2, 2]])
+b = tf.constant([[0, 1], [2, 3]])
+z = tf.matmul(a, b)
+with tf.Session() as sess:
+    z = sess.run(z)
+    print('z:', z)
+
